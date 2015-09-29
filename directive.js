54887cc6bd44d9a8fdbcb24d36434159b2a17ca8
chai <chaihong@jkinfo.com.cn>
2015-09-29 16:53:31 +0800

    number+unit

---
+++ directive.js
@@ -0,0 +1,37 @@
+var APP = angular.module('APP', [])
+	.directive('formatNum', function() {
+		return {
+			scope: {
+			    num:'=' 
+			},
+			restrict: 'AE',
+			replace: true,
+			template: '<div><div>{{dataNum}}</div><div id="b">{{unit}}</div><div>',
+			controller: function($scope) {
+					$scope.data = 10000;
+					var output = '';
+					var round = 10;
+					var symbols = [
+						[Math.pow(10, 16), '京'],
+						[Math.pow(10, 12), '兆'],
+						[Math.pow(10, 8), '億'],
+						[Math.pow(10, 4), '万'],
+						[Math.pow(10, 3), '千'],
+						[Math.pow(10, 2), '百']
+					];
+					for (var i = 0; i < symbols.length; i++) {
+						var sym = symbols[i];
+
+						if ($scope.data / sym[0] >= 1) {
+							output = sym[1] + output;
+							$scope.data /= sym[0];
+						}
+					}
+					$scope.dataNum = (Math.round($scope.data * round) / round);
+					$scope.unit = output;
+			
+			}
+		}
+	})
+
+;
